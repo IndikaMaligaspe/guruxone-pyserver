a55e54a75d8ddea2f17579efedb66118cb3f421e
Indika Maligaspe <k.indika.maligaspe@gmail.com>
2024-09-13 05:38:24 +0400

    added services

--- guruxone/api.py
+++ guruxone/api.py
@@ -8,13 +8,13 @@ The REST API for guruxone-api
 from pathlib import Path
 from typing import Annotated
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from .database import SessionLocal
 from .models import DBMember
 from .schema import Member
-from .services.member_service import get_db_members
+from .services.member_service import get_db_member_by_id, get_db_members
 
 app = FastAPI()
 
@@ -32,3 +32,13 @@ def get_session() -> Session:  # type: ignore
 @app.get("/member", response_model=list[Member])
 def get_all_members(db: Annotated[Session, Depends(get_session)]) -> list[DBMember]:
     return get_db_members(db)
+
+
+@app.get("/member/{id}", response_model=Member)
+def get_meber_by_id(id: int, db: Annotated[Session, Depends(get_session)]) -> DBMember:
+    member = get_db_member_by_id(id, db)
+    if member is None:
+        raise HTTPException(
+            status_code=404, detail=f"No Member with the id {id} found."
+        )
+    return member
--- guruxone/services/member_service.py
+++ guruxone/services/member_service.py
@@ -6,3 +6,7 @@ from ..models import DBMember
 
 def get_db_members(db: Session) -> list[DBMember]:
     return db.execute(select(DBMember)).scalars()
+
+
+def get_db_member_by_id(id: int, db: Session) -> DBMember:
+    return db.get(DBMember, id)
